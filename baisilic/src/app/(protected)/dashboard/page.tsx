e49aca12f0d7393d2b235918bca1a6ece32673e3
pcaboor <pierre.caboor59@gmail.com>
2025-01-22 18:04:40 +0100

    Dashboard UI project page

--- baisilic/src/app/(protected)/dashboard/page.tsx
+++ baisilic/src/app/(protected)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useUser } from '@clerk/nextjs'
+import { ExternalLink, Github } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 import useProject from '~/hooks/use-project'
 
@@ -8,7 +9,35 @@ const DashBoard = () => {
     const { project } = useProject()
     return (
         <div>
-            <h1>{project?.name}</h1>
+            <div className='flex items-center justify-between flex-wrap gap-y-4'>
+                <div className='w-fit rounded-lg bg-primary px-4 py-3'>
+                    <div className='flex items-center'>
+                        <Github className='size-5 text-white' />
+                        <div className='ml-2'>
+                            <p className='text-sm font-medium text-white'>
+                                This project is link to {' '}
+                                <Link href={project?.githubUrl ?? ""} className='inline-flex items-center text-white/80 hover:underline'>
+                                    {project?.githubUrl}
+                                    <ExternalLink className='ml-1 size-4' />
+                                </Link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                {/* Add more components here */}
+                <div className='h-4'></div>
+                <div className='flex items-center gap-4'>
+                    Team
+                </div>
+            </div>
+            <div className='mt-4'>
+                <div className='grid grid-cols-1 gap-4 sm:grid-cols-5'>
+                    Ask
+                </div>
+            </div>
+            <div className='mt-8'>
+                Log
+            </div>
         </div>
     )
 }
